bbbb90a26a2fe18b146ded81993b1c35c4ce0347
Max Westphal <max.westphal@outlook.de>
2023-05-10 00:52:54 +0200

    fixes for CRAN submission

--- R/cases.R
+++ R/cases.R
@@ -1,9 +1,9 @@
 #' \code{cases} package
 #'
-#' Evaluate the accuracy of multiple (candidate) classifiers in several subgroups
+#' Enables simultaneous statistical inference for the accuracy of multiple classifiers in multiple subgroups (strata). For instance, allows to perform multiple comparisons in diagnostic accuracy studies with co-primary endpoints sensitivity and specificity. (Westphal, Max, and Antonia Zapf. "Statistical Inference for Diagnostic Test Accuracy Studies with Multiple Comparisons." <arXiv:2105.13469> (2021).)
 #'
-#' See the README on
-#' \href{https://github.com/maxwestphal/cases}{GitHub}
+#' See the vignettes
+#' vignette()
 #'
 #' @docType package
 #' @name cases
